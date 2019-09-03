2c6a04b69ecb1bf9e09a1fc4d11ab63d94afeb03
Panela Leung <leungpanela@gmail.com>
2019-09-03 02:14:42 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,6 +9,14 @@ class CashRegister
     @items = []
   end
 
+  # class CashRegister
+  #   attr_accessor :total, :discount, :price, :items
+  #
+  #   def initialize(discount = 0)
+  #     @total = 0
+  #     @discount = discount
+  #     @items = []
+  #   end
   def total
     @total
   end
